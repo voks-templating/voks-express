9ff907ff6f58b86e0b75a0af6f200e0e3cfb43ac
Falk Hoppe <falk@hey.com>
2023-06-09 12:21:40 +0200

    add express renderer registration handler for voks

---
+++ examples/express-deno/main.ts
@@ -0,0 +1,40 @@
+import express from "express";
+import {
+  html,
+  type HTMLTemplate,
+  renderToStream,
+  type ResponseStream,
+} from "voks";
+import voksExpress from "../../mod.ts";
+
+const app = express();
+app.use(
+  voksExpress<HTMLTemplate, ResponseStream>(renderToStream, { timeout: 50 }),
+);
+
+app.get("/", (_req: express.Request, res: express.Response) => {
+  const message = "Hello, World!";
+
+  res.render(
+    html`<!DOCTYPE html>
+  <html>
+    <head>
+      <title>Deno Express App for testing VoksExpress</title>
+    </head>
+    <body>
+      <h1>${message}</h1>
+    </body>
+  </html>
+    `,
+    {
+      fallback: () => {
+        console.log("fallback");
+        return `CONTENT WASN'T FAST ENOUUGH, BRO!`;
+      },
+    },
+  );
+});
+
+const server = app.listen(() => {
+  console.log("server started at", `http://localhost:${server.address().port}`);
+});
---
+++ mod.ts
@@ -0,0 +1,43 @@
+type ExpressRequestLike = Record<string | number | symbol, never>;
+type ExpressNextFunctionLike = () => void;
+type ExpressResponseLike<T, R> = {
+  render: (template: T) => Promise<void>;
+  end: () => void;
+} & R;
+
+export type RendererOptions = {
+  timeout?: number;
+};
+
+export default <HTMLTemplate, ResponseStream>(
+  renderToStream: (
+    res: ExpressResponseLike<HTMLTemplate, ResponseStream>,
+    template: HTMLTemplate,
+  ) => Promise<void>,
+  { timeout = 1500 }: RendererOptions = {},
+) =>
+(
+  _req: ExpressRequestLike,
+  res: ExpressResponseLike<HTMLTemplate, ResponseStream>,
+  next: ExpressNextFunctionLike,
+) => {
+  res.render = async (
+    template: HTMLTemplate,
+  ) => {
+    try {
+      await Promise.race([
+        new Promise((_resolve, reject) => {
+          setTimeout(reject, timeout);
+        }),
+        await renderToStream(res, template),
+      ]);
+    } catch (e) {
+      console.log("rendering failed: ", e.message);
+      console.log(e.stack);
+    } finally {
+      res.end();
+    }
+  };
+
+  return next();
+};
